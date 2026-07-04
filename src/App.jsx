6ef195cb844d0ddceabbfe8e6ddaaf26597fda67
Ronaldo <ronaldoon@gmail.com>
2026-07-04 19:44:22 -0300

    feat: add Lenis smooth scroll integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
+import { Lenis } from 'lenis/react'
 import MatrixRain from './components/MatrixRain'
 import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
 import Contact from './components/Contact'
+import 'lenis/dist/lenis.css'
 import './App.css'
 
 function App() {
   return (
-    <>
+    <Lenis root>
       <MatrixRain />
       <Hero />
       <About />
       <Projects />
       <Skills />
       <Contact />
-    </>
+    </Lenis>
   )
 }
 
